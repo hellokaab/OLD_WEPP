718ce90f30bf64c2f85b849328775b5a253e39d8
Pongpan <hellokaab@gmail.com>
2017-08-01 15:17:58 +0700

    edit examing

--- public/js/Components/editExamingCtrl.js
+++ public/js/Components/editExamingCtrl.js
@@ -1,6 +1,218 @@
 /**
  * Created by Pongpan on 27-Jul-17.
  */
-app.controller('editExamingCtrl', ['$scope', '$window', function ($scope, $window) {
+app.controller('editOpenExamingCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.myGroups = $window.myGroup;
+    $scope.sections = $window.sections;
+    $scope.exams = $window.exams;
+    $scope.thisUser = $window.myuser;
+    $scope.examing = findExamingByID($window.examingID);
+    $scope.examExamings = $window.exam_examing;
 
+    // Initial values of this examing
+    $scope.openExamName = $scope.examing.examing_name;
+    $scope.userGroupId = $scope.examing.group_id;
+    $scope.examingMode = $scope.examing.examing_mode;
+    $scope.examingPassword = $scope.examing.examing_pass;
+    $scope.ipMode = $scope.examing.ip_group.length > 0 ? '1' : '0';
+    $scope.gatewayIp = '';
+    $scope.subnetmask = '/32';
+    $scope.allowNetwork = $scope.examing.ip_group;
+
+    $scope.selectExam = [];
+    for (i = 0; i < $scope.examExamings.length; i++)
+        $scope.selectExam.push($scope.examExamings[i].exam_id);
+
+    $scope.randomExam = [];
+    for (i = 1; i <= $scope.examing.amount; i++)
+        $scope.randomExam.push(i);
+    $scope.amountExam = $scope.examing.amount;
+
+    $('#examingBegin').val(dtDBToDtPicker($scope.examing.start_date_time));
+    $('#examingEnd').val(dtDBToDtPicker($scope.examing.end_date_time));
+
+    $(document).ready(function () {
+        $('#group_id').val($scope.userGroupId);
+    });
+    var deleteExamExaming = new Array();
+    //----------------------------------------------------------------------
+    $scope.ticExam = function () {
+        $scope.selectExam = [];
+        $scope.randomExam = [];
+        i = 1;
+        $('[id^=exam_]').each(function () {
+            if ($(this).prop('checked')) {
+                $scope.selectExam.push(parseInt($(this).attr('id').substr(5)));
+                $scope.randomExam.push(i++);
+            }
+        });
+    };
+    //----------------------------------------------------------------------
+    $scope.viewExam = function (examGroupId) {
+        if ($('#group_' + examGroupId).hasClass('fa-plus-square')) {
+            $('#group_' + examGroupId).removeClass('fa-plus-square');
+            $('#group_' + examGroupId).addClass('fa-minus-square');
+            $('#group_' + examGroupId).parent().parent().children('div').show();
+        } else {
+            $('#group_' + examGroupId).removeClass('fa-minus-square');
+            $('#group_' + examGroupId).addClass('fa-plus-square');
+            $('#group_' + examGroupId).parent().parent().children('div').hide();
+        }
+    };
+
+    //----------------------------------------------------------------------
+    $scope.randomPassword = function () {
+        $scope.examingPassword = Math.floor(Math.random() * 9000) + 1000;
+    };
+
+    //----------------------------------------------------------------------
+    $scope.addNetwork = function () {
+        $('#notice_gateway_ip').html('* กรุณาระบุไอพี Gateway ให้ถูกต้อง').show();
+        if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test($scope.gatewayIp)) {
+            $scope.allowNetwork = $scope.allowNetwork.length === 0 ? $scope.allowNetwork : $scope.allowNetwork + '\n';
+            $scope.allowNetwork = $scope.allowNetwork + $scope.gatewayIp + $scope.subnetmask;
+            $scope.gatewayIp = '';
+            $('#notice_gateway_ip').hide();
+        }
+    };
+    //----------------------------------------------------------------------
+    $scope.clearIP = function () {
+        $scope.allowNetwork = '';
+    };
+
+    //----------------------------------------------------------------------
+    $scope.goBack = function () {
+        window.history.back();
+    }
+    //----------------------------------------------------------------------
+    $scope.okEditOpenExam = function () {
+        $('#notice_gateway_ip').hide();
+        $('#notice_examing_time').hide();
+        $('#notice_examing_begin').hide();
+        $('#notice_exam').hide();
+        $('#notice_examing_usr_grp').hide();
+        $('#notice_examing_name').hide();
+        console.log($scope.openExamName != $scope.examing.examing_name);
+        completeExamName = $scope.openExamName.length > 0;
+        completeUserGroup = $scope.userGroupId > 0;
+        completeNoDuplicate = true;
+        if(completeExamName){
+            if ($scope.openExamName != $scope.examing.examing_name){
+                completeNoDuplicate = findExamingByNameAndGroup($scope.openExamName,$scope.userGroupId);
+            }
+
+        }
+        completeExam = $scope.selectExam.length > 0;
+        completeExamingBegin = $('#examingBegin').val().length > 0;
+        completeExamingEnd = $('#examingEnd').val().length > 0;
+
+        completeAllowNetwork = $scope.allowNetwork.length > 0;
+        completeIP = $scope.ipMode === '0' ? true : (completeAllowNetwork ? true : false);
+
+        if (completeExamName && completeUserGroup && completeExam && completeExamingBegin && completeExamingEnd && completeIP && completeNoDuplicate) {
+
+            dateBegin = new Date(dtPickerToDtJs($('#examingBegin').val()));
+            dateEnd = new Date(dtPickerToDtJs($('#examingEnd').val()));
+
+            $('#edit_examing_part').waitMe({
+                effect: 'win8_linear',
+                bg: 'rgba(255,255,255,0.9)',
+                color: '#3bafda'
+            });
+
+            console.log($scope.examExamings);
+            for(i=0;i<$scope.examExamings.length;i++){
+                var EID = $scope.examExamings[i].exam_id;
+                var indexOfStevie = $scope.selectExam.indexOf(EID);
+                if(indexOfStevie == -1){
+                    deleteExamExaming.push(EID)
+                }
+            }
+
+            data = {
+                id: $scope.examing.id,
+                examing_name: $scope.openExamName,
+                group_id: $scope.userGroupId,
+                exam: $scope.selectExam,
+                examing_mode: $scope.examingMode,
+                amount: $scope.examingMode === 'n' ? $scope.selectExam.length : $scope.amountExam,
+                start_date_time: dtJsToDtDB(dateBegin),
+                end_date_time: dtJsToDtDB(dateEnd),
+                examing_pass: $scope.examingPassword,
+                ip_group: $scope.allowNetwork,
+                deleteExamExaming:deleteExamExaming
+            }
+            updateExaming(data);
+
+//             $.post('../Process.php', {
+//                 process: 'edit examing',
+//                 examingId: $scope.examingId,
+//                 name: $scope.openExamName,
+// //                    usrGrp: $scope.userGroupId,
+//                 exam: JSON.stringify($scope.selectExam),
+//                 mode: $scope.examingMode,
+//                 amount: $scope.examingMode === 'n' ? $scope.selectExam.length : $scope.amountExam,
+//                 start: dtJsToDtDB(dateBegin),
+//                 end: dtJsToDtDB(dateEnd),
+//                 pass: $scope.examingPassword,
+//                 ip: $scope.allowNetwork
+//             }, function (data) {
+//                 if (data === '1') {
+//                     window.location.href = 'listOpenExam.php';
+//                 } else {
+//                     $('#open_exam_part').waitMe('hide');
+//                     alert(data);
+//                 }
+//             });
+        } else {
+            if (!completeIP) {
+                $('#notice_gateway_ip').html('* กรุณาระบุเครือข่ายที่อนุณาตให้ทำข้อสอบ').show();
+                $('[ng-model=gatewayIp]').focus();
+            }
+            if (!completeExamingEnd) {
+                $('#notice_examing_end').html('* กรุณาระบุเวลาสิ้นสุดการสอบ').show();
+            }
+            if (!completeExamingBegin) {
+                $('#notice_examing_begin').html('* กรุณาระบุเวลาเริ่มสอบ').show();
+            }
+            if (!completeExam) {
+                $('#notice_exam').html('* กรุณาระบุข้อสอบที่ใช้ในการสอบ').show();
+                $('[ng-model=userGroupId]').focus();
+            }
+            if (!completeUserGroup) {
+                $('#notice_examing_usr_grp').html('* กรุณาระบุกลุ่มเรียน').show();
+                $('[ng-model=userGroupId]').focus();
+            }
+            if (!completeExamName) {
+                $('#notice_examing_name').html('* กรุณาระบุชื่อการสอบ').show();
+                $('[ng-model=openExamName]').focus();
+            }
+
+            if (!completeNoDuplicate) {
+                $('#notice_examing_name').html('* มีการสอบนี้ในกลุ่มเรียนที่เลือกแล้ว').show();
+                $('[ng-model=openExamName]').focus();
+            }
+        }
+    };
+    //----------------------------------------------------------------------
+    function dtPickerToDtJs(date) {
+        dt = date.split(' ');
+        d = dt[0].split('-');
+        jsDt = d[1] + '/' + d[0] + '/' + d[2] + ' ' + dt[1];
+        return jsDt;
+    }
+    function dtJsToDtDB(date) {
+        date = date.toLocaleString();
+        dt = date.split(' ');
+        d = dt[0].split('/');
+        r = (d[2] - 543) + '-' + d[1] + '-' + d[0] + ' ' + dt[1];
+        return r;
+    }
+
+    function dtDBToDtPicker(date) {
+        dt = date.split(' ');
+        d = dt[0].split('-');
+        r = (d[2]) + '-' + d[1] + '-' + d[0] + ' ' + dt[1];
+        return r.substring(0, 16);
+    }
 }]);
--- resources/views/pages/editExaming.blade.php
+++ resources/views/pages/editExaming.blade.php
@@ -3,15 +3,267 @@
     <script src="js/Components/editExamingCtrl.js"></script>
     <script>
         var examingID = {{$examingID}};
+        var myGroup = findMyGroup(myuser).responseJSON;
+        var sections = findSectionSharedToMe(myuser.id);
+        var exams = findExamSharedToMe(myuser.id);
+        var exam_examing = findExamExamingByExamingID(examingID);
     </script>
-    <div ng-controller="editExamingCtrl">
+    <div ng-controller="editOpenExamingCtrl">
         <div class="col-lg-12">
             <div class="panel panel-default" id="edit_examing_part">
                 <div class="panel-heading">
                     <b style="color: #555">แก้ไขการเปิดสอบ</b>
                 </div>
-                <div class="panel-body"></div>
+                <div class="panel-body">
+                    <div class="form-horizontal" role="form">
+                        {{--Examing Name--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">ชื่อการสอบ: <b class="danger">*</b></label>
+                            <div class="col-md-5">
+                                <input type="text" class="form-control" ng-model="openExamName" maxlength="200" autofocus/>
+                                <div class="notice" id="notice_examing_name" style="display: none">กรุณาระบุชื่อการสอบ</div>
+                            </div>
+                        </div>
+
+                        {{--Select Group--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">กลุ่มเรียน: <b class="danger">*</b></label>
+                            <div class="col-md-5">
+                                <select class="form-control" ng-model="userGroupId" id="group_id">
+                                    <option value="0">กรุณาเลือก</option>
+                                    <option ng-repeat="g in myGroups" value="<%g.id%>"><%g.group_name%></option>
+                                </select>
+                                <div class="notice" id="notice_examing_usr_grp" style="display: none">กรุณาระบุกลุ่มเรียน</div>
+                            </div>
+                        </div>
+
+                        {{--Select Exam--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">ข้อสอบ: <b class="danger">*</b></label>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <label class="control-label" style="padding-left: 15px" ng-show="sections.length == 0">ไม่พบข้อมูล</label>
+                                    <div class="col-md-12 checkbox" ng-repeat="s in sections">
+                                        <a href="" ng-click="viewExam(s.id)">
+                                            <i id="group_<%s.id%>" class="fa fa-plus-square"></i> <%s.section_name%>
+                                        </a>
+                                        &nbsp;&nbsp;&nbsp;<b style="font-weight: 500" ng-show="s.user_id != thisUser.id">(<%s.creater%>)</b>
+                                        <div style="padding-left: 50px; padding-top: 5px; display: none" ng-repeat="e in exams" ng-if="e.section_id === s.id">
+                                            <input type="checkbox" id="exam_<%e.id%>" ng-click="ticExam()">
+                                            <label style="padding: 0" for="exam_<%e.id%>" ng-click="ticExam()"><%e.exam_name%></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="notice" id="notice_exam" style="display: none">กรุณาระบุข้อสอบที่ใช้ในการสอบ</div>
+                            </div>
+                        </div>
+
+                        {{--Exam Mode--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">โหมดการสอบ:</label>
+                            <div class="col-md-8">
+                                <div class="radio">
+                                    <div class="col-md-3">
+                                        <input type="radio" name="examingMode" id="normalChk" value="n" ng-model="examingMode">
+                                        <label for="normalChk" style="padding: 0">เรียงตามลำดับ</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <input type="radio" name="examingMode" id="randomChk" value="r" ng-model="examingMode">
+                                        <label for="randomChk" style="padding: 0">สุ่ม</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{--Amount--}}
+                        <div class="form-group" ng-show="examingMode === 'r'">
+                            <label class="col-md-2 control-label">จำนวนข้อ: <b class="danger">*</b></label>
+                            <div class="col-md-3">
+                                <select class="form-control">
+                                    <option value="<%a%>" ng-model="amountExam" ng-repeat="a in randomExam"><%a%></option>
+                                </select>
+                            </div>
+                        </div>
+
+                        {{--Begin Time--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">เวลาเริ่มสอบ <b class="danger">*</b></label>
+                            <div class="col-md-5">
+                                <input type="text" style="background-color: #fff;cursor: pointer" class="form-control examingBegin" id="examingBegin" data-field="datetime" data-startend="start" data-startendelem=".examingEnd" readonly placeholder="คลิกเลือกเวลา"/>
+                                <div class="notice" id="notice_examing_begin" style="display: none">กรุณาระบุเวลาเริ่มสอบ</div>
+                            </div>
+                        </div>
+
+                        {{--End Time--}}
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">เวลาสิ้นสุดการสอบ <b class="danger">*</b></label>
+                            <div class="col-md-5">
+                                <input type="text" style="background-color: #fff;cursor: pointer" class="form-control examingEnd" id="examingEnd" data-field="datetime" data-startend="end" data-startendelem=".examingBegin" readonly placeholder="คลิกเลือกเวลา"/>
+                                <div class="notice" id="notice_examing_end" style="display: none">กรุณาระบุเวลาสิ้นสุดการสอบ</div>
+                            </div>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">รหัสเข้าสอบ</label>
+                            <div class="col-md-5">
+                                <input type="text" class="form-control" ng-model="examingPassword" maxlength="8"/>
+                                <a href="" style="float: right; margin-top: 5px" ng-click="randomPassword()">
+                                    <i class="fa fa-random" aria-hidden="true"></i> สุ่มรหัสผ่าน
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- IP -->
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">จำกัดเฉพาะ IP:</label>
+                            <div class="col-md-8">
+                                <div class="radio">
+                                    <div class="col-md-2">
+                                        <input type="radio" name="limitIp" id="unlimitIP" value="0" ng-model="ipMode">
+                                        <label for="unlimitIP">ไม่ใช่</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <input type="radio" name="limitIp" id="limitIP" value="1" ng-model="ipMode">
+                                        <label for="limitIP">ใช่</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Gateway IP -->
+                        <div class="form-group" ng-show="ipMode === '1'">
+                            <label class="col-md-2 control-label">IP Router/Gateway:</label>
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <input type="text" class="form-control" ng-model="gatewayIp" maxlength="15" placeholder="เช่น 172.17.5.254"/>
+                                        <div class="col-md-12 notice" id="notice_gateway_ip" style="display: none">กรุณาระบุไอพี Gateway ให้ถูกต้อง</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Subnet mask -->
+                        <div class="form-group" ng-show="ipMode === '1'">
+                            <label class="col-md-2 control-label">Subnet Mask:</label>
+                            <div class="col-md-5">
+                                <div>
+                                    <select name="selecter_basic" ng-model="subnetmask" class="form-control">
+                                        <option value="/32">255.255.255.255</option>
+                                        <option value="/31">255.255.255.254</option>
+                                        <option value="/30">255.255.255.252</option>
+                                        <option value="/29">255.255.255.248</option>
+                                        <option value="/28">255.255.255.240</option>
+                                        <option value="/27">255.255.255.224</option>
+                                        <option value="/26">255.255.255.192</option>
+                                        <option value="/25">255.255.255.128</option>
+                                        <option value="/24">255.255.255.0</option>
+                                        <option value="/23">255.255.254.0</option>
+                                        <option value="/22">255.255.252.0</option>
+                                        <option value="/21">255.255.248.0</option>
+                                        <option value="/20">255.255.240.0</option>
+                                        <option value="/19">255.255.224.0</option>
+                                        <option value="/18">255.255.192.0</option>
+                                        <option value="/17">255.255.128.0</option>
+                                        <option value="/16">255.255.0.0</option>
+                                        <option value="/15">255.254.0.0</option>
+                                        <option value="/14">255.252.0.0</option>
+                                        <option value="/13">255.248.0.0</option>
+                                        <option value="/12">255.240.0.0</option>
+                                        <option value="/11">255.224.0.0</option>
+                                        <option value="/10">255.192.0.0</option>
+                                        <option value="/9">255.128.0.0</option>
+                                        <option value="/8">255.0.0.0</option>
+                                        <option value="/7">254.0.0.0</option>
+                                        <option value="/6">252.0.0.0</option>
+                                        <option value="/5">248.0.0.0</option>
+                                        <option value="/4">240.0.0.0</option>
+                                        <option value="/3">224.0.0.0</option>
+                                        <option value="/2">192.0.0.0</option>
+                                        <option value="/1">128.0.0.0</option>
+                                    </select>
+                                    <button ng-click="addNetwork()" class="btn btn-info" style="margin-top: 15px"><i class="fa fa-arrow-down" aria-hidden="true"></i> เพิ่มเครือข่าย</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group" ng-show="ipMode === '1'">
+                            <label class="col-md-2 control-label">เครือข่ายที่ได้รับอนุญาต</label>
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <textarea type="text" class="form-control io_textarea time" ng-model="allowNetwork" maxlength="100" rows="5" disabled></textarea>
+                                        <a href="" style="float: right" ng-click="clearIP()"><i class="fa fa-refresh" aria-hidden="true"></i> ล้างรายการเครือข่าย</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+
+                        <!--Submit part -->
+                        <div class = "form-group">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-3">
+                                <input type="button" class="btn btn-success btn-block" value="บันทึก" ng-click="okEditOpenExam()"/>
+                            </div>
+                            <div class="col-md-3">
+                                <a class="btn btn-danger btn-block" ng-click="goBack()">ยกเลิก</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <b class="danger">*</b> จำเป็นต้องกรอกข้อมูล
+                </div>
             </div>
         </div>
+        <div id="dtBox"></div>
     </div>
+
+    <script>
+        $(document).ready(function () {
+            $('[ng-model=userGroupId]').val(0);
+            setDataTimePart();
+
+            // จัดการติกข้อสอบที่เลือกไว้
+            $('[id^=exam_]').each(function () {
+                examID = $(this).attr('id').substr(5);
+                $(exam_examing).each(function (k, v) {
+                    if (parseInt(examID) === v.exam_id) {
+                        $('#exam_' + examID).attr('checked', 'checked');
+                        $('#exam_' + examID).parent().parent().children('div').show();
+                        $('#exam_' + examID).parent().parent().children().children('.fa-plus-square').addClass('fa-minus-square');
+                        $('#exam_' + examID).parent().parent().children().children('.fa-plus-square').removeClass('fa-plus-square');
+                    }
+                });
+
+            });
+
+            $('#openExam_div').css('display','block');
+        });
+
+        function setDataTimePart() {
+            var bIsPopup = displayPopup();
+
+            $("#dtBox").DateTimePicker({
+                isPopup: bIsPopup,
+                addEventHandlers: function () {
+                    var dtPickerObj = this;
+                    $(window).resize(function () {
+                        bIsPopup = displayPopup();
+                        dtPickerObj.setIsPopup(bIsPopup);
+                    });
+                }
+            });
+        }
+        function displayPopup() {
+            if ($(document).width() >= 768)
+                return true;
+            else
+                return true;
+        }
+    </script>
 @endsection
